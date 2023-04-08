0836e685bf0492db332aca22bc59c1fe9e618139
Michael Ilyin <milyin@gmail.com>
2023-04-09 00:28:12 +0200

    admin page templates

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod error;
 use error::Error;
 use persy::Persy;
 use rocket::{get, routes, Ignite, Rocket};
+use rocket_dyn_templates::Template;
 
 // Define 'index' route
 #[get("/")]
@@ -10,19 +11,42 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
+// Admin page, returns a handlebars template
+#[get("/admin")]
+fn admin() -> Template {
+    let context = ();
+    // Render admin/index.html.hbs template
+    Template::render("admin/index", &context)
+}
+
 async fn init() -> Result<Rocket<Ignite>, Error> {
-    // Get executable name
+    // Get executable name without extension
     let exe_name = std::env::current_exe()?;
+    // Remove extension
+    let db_name = exe_name
+        .file_stem()
+        .map(|s| s.to_str())
+        .flatten()
+        .unwrap_or("gameserver");
     // make db name = executable name + ".db"
-    let db_name = exe_name.to_str().unwrap().to_owned() + ".db";
-    // create Persy database storage
-    Persy::create(db_name)?;
+    let db_name = db_name.to_owned() + ".db";
+    // create or open Persy database storage
+    println!("Database file: {}", db_name);
+    let config = persy::Config::default();
+    Persy::open_or_create_with(db_name, config, |_persy| Ok(()))?;
     // Start rocket server
-    let rocket = rocket::build().mount("/", routes![index]).launch().await?;
+    let rocket = rocket::build()
+        // Attach Template::fairing() to rocket instance
+        .attach(Template::fairing())
+        // Mount index route
+        .mount("/", routes![index])
+        // Mount admin route
+        .mount("/", routes![admin])
+        .launch()
+        .await?;
     Ok(rocket)
 }
 
-// Init rocket server in a modern, asynchronous way
 #[rocket::main]
 async fn main() {
     // Handle result
